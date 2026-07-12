7abae3d8c2ffde3a3a0dac23212f5b30885da0cb
Hamza Osama <hamzaosamaa12@gmail.com>
2026-07-12 11:06:42 +0300

    packages and a simple server

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-const fs = require("fs")
-fs.readFile("./test.txt", "utf-8", (err, data) => {
-    if (err){
-        console.error(err)
-    }
-    console.log(data)
-})
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const qs = require("querystring")
+
+var filepath = path.resolve(__dirname, "products.json")
+if (!fs.existsSync(filepath)){
+    fs.appendFileSync(filepath, '[]');
+}
+
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World');
+}).listen(8081);
+
+console.log('Server running at http://127.0.0.1:8081/');
