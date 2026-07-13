5f55a20feda6b6fa35ef3c484d140e70b55f6b93
Hamza Osama <hamzaosamaa12@gmail.com>
2026-07-13 09:52:11 +0300

    first endpoint

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ const express = require("express")
 var app = express();
 const PORT = 3000;
 app.listen(PORT, "localhost", ()=>{
-    console.log(`Server running on localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
+})
+
+app.get("/", (req, res) => {
+    res.status(200)
+    res.header({"Content-Type": "text/html"})
+    res.end("<h1>Hello</h1>")
 })
